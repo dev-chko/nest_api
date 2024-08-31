49b13f89993ef3ca8c1a1b7ca34e39beed38baac
dev-chko <oko0412o@naver.com>
2024-08-31 19:46:21 +0900

    change express inteface

--- src/users/middlewares/currentuser.middleware.ts
+++ src/users/middlewares/currentuser.middleware.ts
@@ -1,6 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../users.entity';
+import * as request from 'supertest';
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -10,7 +20,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
     const { userId } = req.session || {};
     if (userId) {
       const user = await this.usersService.findOne(userId);
-      // @ts-ignore
       req.currentUser = user;
     }
     next();
